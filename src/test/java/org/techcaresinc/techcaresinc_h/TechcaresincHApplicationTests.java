021a875b58f4334a7697e1dce004615c7a5ae0e5
Ashf13 <somar9474@gmail.com>
2019-09-13 14:00:34 +0300

    Initial commit

---
+++ src/test/java/org/techcaresinc/techcaresinc_h/TechcaresincHApplicationTests.java
@@ -0,0 +1,16 @@
+package org.techcaresinc.techcaresinc_h;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TechcaresincHApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
